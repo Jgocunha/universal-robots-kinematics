cced23f6871ff9c497e14348e7125682eb86af27
Jgocunha <gasparcunha19@outlook.pt>
2020-10-26 22:55:21 +0000

    printing matrices to .txt files

--- MATLAB files/printM.m
+++ MATLAB files/printM.m
@@ -4,26 +4,25 @@ disp('Program started');
 
 syms j [1 6]; % joints
 syms d [1 7]; % link dimensions x
-syms a [2 5]; % link dimensions z
+syms a [1 5]; % link dimensions z
 
 dof=6; % degrees of freedom UR10
 
 iMfile='iMfile.txt'; % individual matrices output file
 cMfile='cMfile.txt'; % compound matrices output file
 
-iMfile='iMfile.xlsx'; % individual matrices output file
-cMfile='cMfile.xlsx'; % compound matrices output file
+aux_matrix=strings(4,4); %auxiliary matrix of strings to correctly output the matrices
 
 %Definition of the modified Denavit-Hartenberg matrix (Do not change!)
-DHMatrix = [ 0         0       d(1)   j1;    % 1  0T1 
-            -90        0       d(2)   j2-90; % 2  1T2 
-             0         a(2)    d(3)   j3;    % 3  2T3 
-             0         a(3)    d(4)   j4;    % 4  3T4
-             0         a(4)    d(5)   90;    % 4' 4T4' 5
-             90        0       0      j5;    % 5  4'T5 6
-            -90        0       0      -90;   % 5' 5T5' 7
-             0         a(5)    d(6)   j6;    % 6  5'T6 8
-             0         0       d(7)     0;]; % 7  6T7  9
+DHMatrix = [ 0         0       d1   j1;    % 1  0T1 
+            -90        0       d2   j2-90; % 2  1T2 
+             0         a2      d3   j3;    % 3  2T3 
+             0         a3      d4   j4;    % 4  3T4
+             0         a4      d5   90;    % 4' 4T4' 5
+             90        0       0    j5;    % 5  4'T5 6
+            -90        0       0    -90;   % 5' 5T5' 7
+             0         a5      d6   j6;    % 6  5'T6 8
+             0         0       d7   0;];   % 7  6T7  9
 
 %% Main program
 
@@ -32,16 +31,31 @@ numFrames=size(DHMatrix);
 numFrames=numFrames(1);
 
 % Compute forward kinematics
-M=fwdKin(DHMatrix)
+M=fwdKin(DHMatrix);
 
-M{1}{1}
-
-%w=char(M{1}{1})
-writematrix(M{1}{1},iMfile)
 % Output matrices
 disp('Outputing forward kinematics solution to iMfile.txt and cMfile.txt');
-%writecell((M{1}),iMfile);
-%writecell(sym2cell(M{1}),iMfile);
-%writecell(sym2cell(M{2}),cMfile);
+
+writematrix("Individual transformation matrices",iMfile);
+for i = 1 : numFrames
+     writematrix(strcat(num2str(i-1),'T',num2str(i)),iMfile,'WriteMode','append');
+     for j = 1 : 4
+        for k = 1 : 4
+            aux_matrix(j,k)=char(M{1}{i}(j,k));
+        end
+     end
+     writematrix(aux_matrix,iMfile,'WriteMode','append');
+end
+
+writematrix("Compound transformation matrices",cMfile);
+for i = 1 : numFrames
+     writematrix(strcat(num2str(0),'T',num2str(i)),cMfile,'WriteMode','append');
+     for j = 1 : 4
+        for k = 1 : 4
+            aux_matrix(j,k)=char(M{2}{i}(j,k));
+        end
+     end
+     writematrix(aux_matrix,cMfile,'WriteMode','append');
+end
 
 disp('Program ended');
