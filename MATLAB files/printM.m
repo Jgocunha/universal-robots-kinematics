e3c1f8637c831d5bdcfd09a51defbc24fbbee7c4
Jgocunha <gasparcunha19@outlook.pt>
2020-10-27 10:11:13 +0000

    using simplify for printM file

--- MATLAB files/printM.m
+++ MATLAB files/printM.m
@@ -8,19 +8,19 @@ syms a [1 5]; % link dimensions z
 
 dof=6; % degrees of freedom UR10
 
-iMfile='iMfile.txt'; % individual matrices output file
-cMfile='cMfile.txt'; % compound matrices output file
+iMfile='.\MatrixOutput\iMfile.txt'; % individual matrices output file
+cMfile='.\MatrixOutput\cMfile.txt'; % compound matrices output file
 
 aux_matrix=strings(4,4); %auxiliary matrix of strings to correctly output the matrices
 
 %Definition of the modified Denavit-Hartenberg matrix (Do not change!)
 DHMatrix = [ 0         0       d1   j1;    % 1  0T1 
-            -90        0       d2   j2-90; % 2  1T2 
+            -pi/2      0       d2   j2-pi/2; % 2  1T2 
              0         a2      d3   j3;    % 3  2T3 
              0         a3      d4   j4;    % 4  3T4
-             0         a4      d5   90;    % 4' 4T4' 5
-             90        0       0    j5;    % 5  4'T5 6
-            -90        0       0    -90;   % 5' 5T5' 7
+             0         a4      d5   pi/2;    % 4' 4T4' 5
+             pi/2      0       0    j5;    % 5  4'T5 6
+            -pi/2      0       0    -pi/2;   % 5' 5T5' 7
              0         a5      d6   j6;    % 6  5'T6 8
              0         0       d7   0;];   % 7  6T7  9
 
@@ -41,7 +41,7 @@ for i = 1 : numFrames
      writematrix(strcat(num2str(i-1),'T',num2str(i)),iMfile,'WriteMode','append');
      for j = 1 : 4
         for k = 1 : 4
-            aux_matrix(j,k)=char(M{1}{i}(j,k));
+            aux_matrix(j,k)=char(simplify(M{1}{i}(j,k)));
         end
      end
      writematrix(aux_matrix,iMfile,'WriteMode','append');
@@ -52,7 +52,8 @@ for i = 1 : numFrames
      writematrix(strcat(num2str(0),'T',num2str(i)),cMfile,'WriteMode','append');
      for j = 1 : 4
         for k = 1 : 4
-            aux_matrix(j,k)=char(M{2}{i}(j,k));
+            
+            aux_matrix(j,k)=char(simplify(M{2}{i}(j,k)));
         end
      end
      writematrix(aux_matrix,cMfile,'WriteMode','append');
