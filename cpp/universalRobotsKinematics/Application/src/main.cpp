ca07439ca6df2f1c40ee3c4d60b269353406e9a9
Jgocunha <gasparcunha19@outlook.pt>
2021-10-11 19:15:51 +0100

    inverse kinematics function working

--- cpp/universalRobotsKinematics/Application/src/main.cpp
+++ cpp/universalRobotsKinematics/Application/src/main.cpp
@@ -26,9 +26,12 @@ int main()
 	//const Eigen::Matrix<float, 1, 6> targetTipPose = { 0.2982f, 0.6746f, 0.7211f, mathLib::rad(180), mathLib::rad(45), mathLib::rad(90) };
 	const Eigen::Matrix<float, 1, 6> targetTipPose = { 0.3835f, 0.3730f, 0.9581f, mathLib::rad(-30.5348f), mathLib::rad(28.4603f), mathLib::rad(47.5405f) }; // mathLib::rad(23), mathLib::rad(345), mathLib::rad(78), mathLib::rad(66), mathLib::rad(77), mathLib::rad(12)
 	// compute inverse kinematics 
-	//float ikSols[8][robot.m_numDoF] =
-	robot.inverseKinematics(targetTipPose);
-		
+	//float ikSols[8][6] = 
+		robot.inverseKinematics(targetTipPose);
+	
+	//for (unsigned int i = 0; i < robot.m_numIkSol; i++)
+	//	std::cout << "IK solution " << i << ": " << mathLib::deg(ikSols[i][0]) << " " << mathLib::deg(ikSols[i][1]) << " " << mathLib::deg(ikSols[i][2]) << " " <<
+	//	mathLib::deg(ikSols[i][3]) << " " << mathLib::deg(ikSols[i][4]) << " " << mathLib::deg(ikSols[i][5]) << std::endl;
 
 	
 	//std::cout << robot.inverseKinematics(targetTipPose)[0][0];
