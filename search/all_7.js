759e01500bec6849f957194a859e1e29a766e4da
Jgocunha <Jgocunha@users.noreply.github.com>
2026-07-11 23:05:42 +0000

    deploy: 29b5e0b189fcde8ad0ca2f34c50c382c70f55dee

--- search/all_7.js
+++ search/all_7.js
@@ -4,5 +4,5 @@ var searchData=
   ['in_20seconds_1',['Average Computation Times (in seconds)',['../index.html#autotoc_md9',1,'']]],
   ['inversekinematics_2',['inverseKinematics',['../classuniversalRobots_1_1UR.html#a1b212a991d947d884cded3f90bc9aa5f',1,'universalRobots::UR']]],
   ['isposereachable_3',['isPoseReachable',['../classuniversalRobots_1_1UR.html#ad0c21958d3a1b2dfb3913c0e7f78ddb7',1,'universalRobots::UR']]],
-  ['issolutionvalid_4',['isSolutionValid',['../classuniversalRobots_1_1UR.html#a7a7b86348c7985e5a8254cdb401fcae4',1,'universalRobots::UR']]]
+  ['issolutionvalid_4',['isSolutionValid',['../classuniversalRobots_1_1UR.html#a18ad4429c5287e8e11e423a59d1fb4a9',1,'universalRobots::UR']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['inversekinematics_0',['inverseKinematics',['../classuniversalRobots_1_1UR.html#a1b212a991d947d884cded3f90bc9aa5f',1,'universalRobots::UR']]],
   ['isposereachable_1',['isPoseReachable',['../classuniversalRobots_1_1UR.html#ad0c21958d3a1b2dfb3913c0e7f78ddb7',1,'universalRobots::UR']]],
-  ['issolutionvalid_2',['isSolutionValid',['../classuniversalRobots_1_1UR.html#a7a7b86348c7985e5a8254cdb401fcae4',1,'universalRobots::UR']]]
+  ['issolutionvalid_2',['isSolutionValid',['../classuniversalRobots_1_1UR.html#a18ad4429c5287e8e11e423a59d1fb4a9',1,'universalRobots::UR']]]
 ];
