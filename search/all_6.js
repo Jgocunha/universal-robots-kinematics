42ce01f468252d065d144e828014b3ab3cb7280a
Jgocunha <Jgocunha@users.noreply.github.com>
2026-07-15 22:34:21 +0000

    deploy: 6806ea8ddd6efd81bee41c741e51ad6d2615c758

--- search/all_6.js
+++ search/all_6.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['generaterandomreachablepose_0',['generateRandomReachablePose',['../classuniversalRobots_1_1UR.html#af7ce3a29ec07a1547f3437c90711d2ef',1,'universalRobots::UR']]],
+  ['generaterandomreachablepose_0',['generaterandomreachablepose',['../classuniversalRobots_1_1UR.html#af7ce3a29ec07a1547f3437c90711d2ef',1,'universalRobots::UR::generateRandomReachablePose() const'],['../classuniversalRobots_1_1UR.html#af5c5d2920266e3763a4f39594b31ced7',1,'universalRobots::UR::generateRandomReachablePose(unsigned int seed) const']]],
   ['getrobottype_1',['getRobotType',['../classuniversalRobots_1_1UR.html#a78d3e71474917ef40ba132ac4fb8563f',1,'universalRobots::UR']]],
   ['googletest_20automatic_2',['GoogleTest (automatic)',['../index.html#autotoc_md5',1,'']]]
 ];
--- search/functions_3.js
+++ search/functions_3.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['generaterandomreachablepose_0',['generateRandomReachablePose',['../classuniversalRobots_1_1UR.html#af7ce3a29ec07a1547f3437c90711d2ef',1,'universalRobots::UR']]],
+  ['generaterandomreachablepose_0',['generaterandomreachablepose',['../classuniversalRobots_1_1UR.html#af7ce3a29ec07a1547f3437c90711d2ef',1,'universalRobots::UR::generateRandomReachablePose() const'],['../classuniversalRobots_1_1UR.html#af5c5d2920266e3763a4f39594b31ced7',1,'universalRobots::UR::generateRandomReachablePose(unsigned int seed) const']]],
   ['getrobottype_1',['getRobotType',['../classuniversalRobots_1_1UR.html#a78d3e71474917ef40ba132ac4fb8563f',1,'universalRobots::UR']]]
 ];
